385adf64bad00c61f7c80bd68f0dbb61f08cfad6
Manoj R <Manoj@BCTCL-DL5420526.bctchn.local>
2022-12-26 15:17:07 +0530

    first commit

---
+++ src/main/java/com/javaawssdk/JavaAwsSdkApplication.java
@@ -0,0 +1,17 @@
+package com.javaawssdk;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAwsSdkApplication  {
+	public static void main(String[] args) {
+		
+		ListUsers.main(args);
+		DescribeInstances.main(args);
+		
+			
+	
+		
+	}
+
+}
