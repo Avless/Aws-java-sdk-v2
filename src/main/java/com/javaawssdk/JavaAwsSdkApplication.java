4836d2516741eaab0cf81fdcd92fb4f6aa82ee11
Manoj R <Manoj@BCTCL-DL5420526.bctchn.local>
2022-12-27 16:59:13 +0530

    cleaned console

--- src/main/java/com/javaawssdk/JavaAwsSdkApplication.java
+++ src/main/java/com/javaawssdk/JavaAwsSdkApplication.java
@@ -1,10 +1,20 @@
 package com.javaawssdk;
 
+
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+
+
 
 @SpringBootApplication
 public class JavaAwsSdkApplication  {
 	public static void main(String[] args) {
+		Logger root=(Logger) LoggerFactory.getILoggerFactory().getLogger(Logger.ROOT_LOGGER_NAME);
+		root.setLevel(Level.ERROR);
 		
 		ListUsers.main(args);
 		DescribeInstances.main(args);
--- src/main/java/com/javaawssdk/S3BucketOps.java
+++ src/main/java/com/javaawssdk/S3BucketOps.java
@@ -1,17 +1,21 @@
 package com.javaawssdk;
 
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.core.waiters.WaiterResponse;
+//import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+//import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+//import software.amazon.awssdk.services.s3.model.S3Exception;
+//import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+//import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
 import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
-import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+//import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+//import software.amazon.awssdk.services.s3.waiters.S3Waiter;
 // snippet-end:[s3.java2.s3_bucket_ops.import]
 // snippet-start:[s3.java2.s3_bucket_ops.main]
 
@@ -26,7 +30,7 @@ import software.amazon.awssdk.services.s3.waiters.S3Waiter;
 public class S3BucketOps {
 
     public static void main(String[] args) {
-
+    	
         // snippet-start:[s3.java2.s3_bucket_ops.region]
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
         Region region = Region.US_EAST_1;
@@ -36,37 +40,37 @@ public class S3BucketOps {
             .build();
 
         // snippet-end:[s3.java2.s3_bucket_ops.region]
-        String bucket = "bucket" + System.currentTimeMillis();
-        System.out.println(bucket);
-        createBucket(s3, bucket);
+        String bucket = "bucket" ; //(ystem.currentTimeMillis());
+//        System.out.println(bucket);
+//        createBucket(s3, bucket);
         performOperations(s3, bucket) ;
         }
 
     // snippet-start:[s3.java2.s3_bucket_ops.create_bucket]
     // Create a bucket by using a S3Waiter object
-    public static void createBucket( S3Client s3Client, String bucketName) {
+//    public static void createBucket( S3Client s3Client, String bucketName) {
 
-        try {
-            S3Waiter s3Waiter = s3Client.waiter();
-            CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
-                .bucket(bucketName)
-                .build();
-
-            s3Client.createBucket(bucketRequest);
-            HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
-                .bucket(bucketName)
-                .build();
-
-            // Wait until the bucket is created and print out the response.
-            WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
-            waiterResponse.matched().response().ifPresent(System.out::println);
-//            System.out.println(bucketName +" is ready");
-
-        } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
-            System.exit(1);
-        }
-    }
+//        try {
+//            S3Waiter s3Waiter = s3Client.waiter();
+//            CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+//                .bucket(bucketName)
+//                .build();
+//
+//            s3Client.createBucket(bucketRequest);
+//            HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+//                .bucket(bucketName)
+//                .build();
+//
+//            // Wait until the bucket is created and print out the response.
+//            WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
+//            waiterResponse.matched().response().ifPresent(System.out::println);
+////            System.out.println(bucketName +" is ready");
+//
+//        } catch (S3Exception e) {
+//            System.err.println(e.awsErrorDetails().errorMessage());
+//            System.exit(1);
+//        }
+//    }
     // snippet-end:[s3.java2.s3_bucket_ops.create_bucket]
 
     public static void performOperations(S3Client s3, String bucket) {
@@ -80,13 +84,13 @@ public class S3BucketOps {
 
         // Delete empty bucket.
         // snippet-start:[s3.java2.s3_bucket_ops.delete_bucket]      
-        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder()
-            .bucket(bucket)
-            .build();
-
-        s3.deleteBucket(deleteBucketRequest);
-        s3.close();
-        // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket] 
+//        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder()
+//            .bucket(bucket)
+//            .build();
+//
+//        s3.deleteBucket(deleteBucketRequest);
+//        s3.close();
+//        // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket] 
     }
     // snippet-end:[s3.java2.s3_bucket_ops.main]
 }
